3fd72ccf12ddf92c4ee5a584e37bdeab559ca13b
dev-rahul-0 <rs650417@gmail.com>
2023-10-03 09:06:19 -0700

    Added minor updates

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Layout extends StatefulWidget {
   const Layout({super.key});
@@ -8,6 +9,9 @@ class Layout extends StatefulWidget {
 }
 
 class _LayoutState extends State<Layout> {
+  bool value = false;
+  bool hide = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,11 +21,12 @@ class _LayoutState extends State<Layout> {
           child: Column(
             children: <Widget>[
               const SizedBox(
-                height: 15,
+                height: 5,
               ),
-              Image.network('https://lpcps.org.in/images/logo-wide.png'),
+              Image.network(
+                  'https://1000logos.net/wp-content/uploads/2021/04/Adobe-logo.png'),
               const SizedBox(
-                height: 50,
+                height: 10,
               ),
               Padding(
                 padding: const EdgeInsets.only(left: 20),
@@ -63,18 +68,219 @@ class _LayoutState extends State<Layout> {
                         padding: EdgeInsets.only(left: 5, right: 20),
                         child: TextField(
                           decoration: InputDecoration(
-                              hintText: 'E-mail or Phone number'),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                              ),
+                              hintText: 'E-mail or Phone number',
+                              prefixIcon: Icon(Icons.email, color: Colors.red)),
                         ),
                       ),
                       const SizedBox(
                         height: 10,
                       ),
-                      const Padding(
+                      Padding(
                         padding: EdgeInsets.only(left: 5, right: 20),
                         child: TextField(
-                          decoration: InputDecoration(hintText: 'Password'),
+                          obscureText: hide,
+                          decoration: InputDecoration(
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                              ),
+                              hintText: 'Password',
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Colors.red,
+                              ),
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    hide = !hide;
+                                  });
+                                },
+                                icon: Icon(hide
+                                    ? Icons.visibility_off
+                                    : Icons.visibility),
+                                color: Colors.red,
+                              )),
                         ),
                       ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        children: [
+                          Checkbox(
+                              value: value,
+                              fillColor: MaterialStatePropertyAll(Colors.white),
+                              checkColor: Colors.red,
+                              onChanged: (value) {
+                                setState(() {
+                                  this.value = value!;
+                                });
+                              }),
+                          const Text(
+                            'Remember Me',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 13,
+                                color: Colors.black),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 140),
+                            child: Text(
+                              'Forget Password',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 10,
+                                  color: Colors.red),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      SizedBox(
+                        height: 45,
+                        width: MediaQuery.of(context).size.width,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 18),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red, elevation: 10),
+                            child: const Text(
+                              'Log In',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text('or'),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.red,
+                                    elevation: 10,
+                                    fixedSize: const Size(100, 45)),
+                                child: const FaIcon(
+                                  FontAwesomeIcons.google,
+                                  color: Colors.white,
+                                )),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 140),
+                            child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.red,
+                                    elevation: 10,
+                                    fixedSize: const Size(100, 45)),
+                                child: const FaIcon(
+                                  FontAwesomeIcons.microsoft,
+                                  color: Colors.white,
+                                )),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            child: const Divider(
+                              color: Colors.red,
+                              thickness: 2,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text('Admin'),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            child: const Divider(
+                              color: Colors.red,
+                              thickness: 2,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Privacy Policy',
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.w500),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            '•',
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.w500),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Contact University',
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.w500),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            '•',
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 15),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Terms of Use',
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Text('Made by Rahul sharma',
+                          style: TextStyle(fontSize: 10, color: Colors.grey)),
                     ],
                   ),
                 ),
